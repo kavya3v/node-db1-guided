3e5e3b56642c502748b38d10927e6ccd10b4088c
Jason Maurer <jsonmaur@fastmail.com>
2020-12-15 19:37:53 -0700

    Code from guided lecture

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,82 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+	try {
+		// translates to `SELECT * FROM messages;`
+		const messages = await db.select("*").from("messages")
+		res.json(messages)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
+	try {
+		// translates to `SELECT * FROM messages WHERE id = ? LIMIT 1;`
+		const [message] = await db
+			.select("*")
+			.from("messages")
+			.where("id", req.params.id)
+			.limit(1)
 
+		res.json(message)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+	try {
+		// use a payload object, just in case `req.body` contains an ID or dates or
+		// something we don't want to be sent to the database
+		const payload = {
+			// database automatically generates the ID and the dates
+			title: req.body.title,
+			contents: req.body.contents,
+		}
 
+		// translates to `INSERT INTO messages (title, contents) VALUES (?, ?);`
+		// knex returns the newly generated ID from the insert command
+		const [id] = await db.insert(payload).into("messages")
+		
+		// query the newly updated row so we can return it
+		const message = await db("messages").first().where("id", id)
+		res.status(201).json(message)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		}
 
+		// translates to `UPDATE messages SET title = ? AND contents = ? WHERE id = ?;`
+		await db("messages").where("id", req.params.id).update(payload)
+
+		// query the newly updated row so we can return it
+		const message = await db("messages").where("id", req.params.id).first()
+		res.json(message)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
+	try {
+		// translates to `DELETE FROM messages WHERE id = ?;`
+		await db("messages").where("id", req.params.id).del()
 
+		// since we no longer have a resource to return, just send a 204
+		// which means success, but no response data is being sent back.
+		res.status(204).end()
+	} catch (err) {
+		next(err)
+	}
 })
 
 module.exports = router
